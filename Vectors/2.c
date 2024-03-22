ad34b185dfc63361b6c9472e4ddf1fbfa72d2e6d
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-03-23 00:20:09 +0300

    Create 2.c

---
+++ Vectors/2.c
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+void del(std::vector<int> & vec) {
+    std::vector<int>::iterator i = vec.begin();
+    while (i  != vec.end()) {
+        i = vec.erase(i);
+        if (i != vec.end()) ++i;
+    }
+}
+
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    del(numbers);
+
+    for (int i = 0; i < numbers.size(); ++i) {
+        std::cout << numbers[i] << " ";
+    }
+
+    return 0;
+}
+    
