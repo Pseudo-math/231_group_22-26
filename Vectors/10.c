1eeb680f8910aadd9ee431f84f6572054ec93a01
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-03-23 02:20:37 +0300

    Create 10.c

---
+++ Vectors/10.c
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void find_index(std::vector<double> & vec, double x, int & i, int & j) {
+    auto lower = std::lower_bound(vec.begin(), vec.end(), x);
+    auto elem = std::find(vec.begin(), vec.end(), x);
+    if (elem != vec.end()) {
+        i = distance(vec.begin(), elem);
+        j = i;
+    }
+    if (lower == vec.end()) {
+        i = vec.size() - 1;
+        j = vec.size();
+    } else if (lower == vec.begin()) {
+        i = -1;
+        j = 0;
+    } else {
+        i = std::distance(vec.begin(), lower - 1);
+        j = std::distance(vec.begin(), lower);
+    }
+}
+
+int main() {
+    std::vector<double> vec = {1.0, 2.0, 3.0, 5.0, 7.0, 9.0};
+    double x = 4.0;
+    int i, j;
+
+    find_index(vec, x, i, j);
+
+    std::cout << i << " " << j << std::endl;
+
+    return 0;
+}
