1a186a5271c860ed134a7088bc6e75f3c5e78437
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-03-23 12:48:56 +0300

    Update All_tasks.c

--- String/All_tasks.c
+++ String/All_tasks.c
@@ -25,3 +25,25 @@ int main() {
     std::cout << IP_check(s);
     return 0;
 }
+--------------------------------------------------------------------
+#include <iostream>
+#include <string>
+
+std::string find_substring(std::string str1, std::string str2) {
+    std::string result = str1;
+    int len = str2.length();
+    int i = 0;
+    i = result.find(str2,i);
+    while (i != std::string::npos) {
+        result.insert(i, "(");
+        result.insert(i + len + 1, ")");
+        i = result.find(str2, i + len + 3);
+    }
+    return result;
+}
+
+int main() {
+    std::string str1 = "3000300233.300.1", str2 = "300";
+    std::cout << find_substring(str1, str2);
+    return 0;
+}
