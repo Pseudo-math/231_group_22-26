ae4eabac35e36f3f8f0615e2f48a71c85d65a8ad
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-04-06 13:42:46 +0300

    Create allTask.cpp

---
+++ structures/allTask.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct Polynomial
+{
+    int deg;
+    float* coeficents;
+    Polynomial()
+    {
+        
+        deg = 0;
+        coeficents = new float[1];
+        coeficents[0] = 0;
+    }
+    Polynomial(int n, float arr[])
+    {
+        
+        deg = n;
+        coeficents = new float[n + 1];
+        for (int i = 0; i <= deg; ++i) coeficents[i] = arr[i];
+    }
+    double Value(double point)
+    {
+        double s = coeficents[deg];
+        for (int i = 1; i <= deg; ++i)
+        {
+            s *= point; 
+            s += coeficents[deg - i];
+        }
+        return s;
+    }
+};
