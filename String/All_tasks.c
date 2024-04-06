a19b05e2204d86622601dac40913866d9a04059a
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-04-06 11:56:40 +0300

    Update All_tasks.c

--- String/All_tasks.c
+++ String/All_tasks.c
@@ -47,3 +47,57 @@ int main() {
     std::cout << find_substring(str1, str2);
     return 0;
 }
+----------------------------------------------------------------------
+#include <iostream>
+#include <string>
+using namespace std;
+string Sumator(string str)
+{
+    int number[2] = {0, 0};
+    int index = str.length();
+    int j = 0;
+    string temp = "";
+    string result = str;
+    for (int i = 0; i < str.length(); ++i)
+    {
+        temp = str.substr(i, 1);
+        
+        if (isdigit(str[i])) 
+        {
+            number[j] = number[j] * 10 + stoi(temp);
+            if (index > i) index = i;
+            
+        }
+        else if (str[i] == '+') 
+        {
+            j = 1;
+        }
+        else 
+        {
+            //cout << number[0] + number[1] << " ";
+            temp  = to_string(number[0] + number[1]);
+            if (j) result.replace(result.length() - (str.length() - index),  i - index, temp);
+            j = 0;
+            index = str.length();
+            number[0] = 0;
+            number[1] = 0;
+        }
+    }
+    if (j == 1)
+    {
+        temp  = to_string(number[0] + number[1]);
+        if (j) result.replace(result.length() - (str.length() - index),  str.length() - index, temp);
+        j = 0;
+        index = str.length();
+        number[0] = 0;
+        number[1] = 0;
+    }
+    return result;
+}
+
+int main()
+{
+    cout<< Sumator("5+26-72+35gh32+45");
+
+    return 0;
+}
