f34a307ab0138a48a07348fbec1e9dab5df79b6f
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-04-20 11:54:52 +0300

    Create stackArray.cpp

---
+++ stacks/stackArray.cpp
@@ -0,0 +1,43 @@
+struct Stack {
+    int size;
+    int *elements;
+
+    public Stack()
+    {
+       elements = new List<T>();
+       size = 0;
+    } 
+
+    public int Size() 
+    {
+       return size;
+    }
+
+    public T Pop()
+    {
+       if (size != 0)
+       {
+           T result = elements[size - 1];
+           elements.RemoveAt(size - 1);
+           --size;
+           return result;
+       }
+       return default(T); // null, если стек пустой
+    }
+	  
+    public void Push(T val)
+    {
+       elements.Add(val);
+       ++size;
+    }
+
+    public T Peek()
+    {
+      if (size != 0) return elements[size - 1];
+      return default(T); // null, если стек пустой
+    }
+   }
+
+}
+
+
